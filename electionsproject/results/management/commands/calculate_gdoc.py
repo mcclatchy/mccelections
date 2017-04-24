35045981ff35122c24ae9039f9df9d87ec4450a5
Greg Linch <greglinch@users.noreply.github.com>
2017-04-24 14:04:02 -0400

    update calc gdoc to make sure it's only trying to calculate objects that have values
    it would previously raise an exception if you're importing null values for vote count or precincts reporting

--- electionsproject/results/management/commands/calculate_gdoc.py
+++ electionsproject/results/management/commands/calculate_gdoc.py
@@ -7,77 +7,81 @@ from results.calculations import calculated_percent
 
 
 def calculate_precinctsreportingpct(electiondate_arg):
-    gdoc_results = ResultManual.objects.filter(electiondate=electiondate_arg, gdoc_import=True)
+    gdoc_results = ResultManual.objects.filter(electiondate=electiondate_arg, gdoc_import=True, precinctsreporting__gt=0)
 
-    message = 'PRECINTS REPORTING PERCENT'
-    slackbot(message)
-    start_time = timezone.localtime(timezone.now())
-    start_message = 'Started calculations:\t' + str(unicode(start_time))
-    message = start_message
-    slackbot(message)
+    if gdoc_results > 0:
 
-    for result in gdoc_results:
-        precincts_reporting = result.precinctsreporting
-        precincts_total = result.precinctstotal
-        precincts_reporting_pct = result.precinctsreportingpct
+        message = 'PRECINTS REPORTING PERCENT'
+        slackbot(message)
+        start_time = timezone.localtime(timezone.now())
+        start_message = 'Started calculations:\t' + str(unicode(start_time))
+        message = start_message
+        slackbot(message)
 
-        if precincts_reporting and precincts_total:
-            precincts_reporting_pct = calculated_percent(precincts_reporting, precincts_total)
-        elif result.precinctsreporting == None:
-            precincts_reporting_pct = None
+        for result in gdoc_results:
+            precincts_reporting = result.precinctsreporting
+            precincts_total = result.precinctstotal
+            precincts_reporting_pct = result.precinctsreportingpct
 
-        result.save()
+            if precincts_reporting and precincts_total:
+                precincts_reporting_pct = calculated_percent(precincts_reporting, precincts_total)
+            elif result.precinctsreporting == None:
+                precincts_reporting_pct = None
 
-    end_time = timezone.localtime(timezone.now())
-    end_message = '\nFinished calculations:\t' + str(unicode(end_time)) + '\n'
-    import_length = str(unicode(end_time - start_time)) 
-    message = end_message
-    slackbot(message)
-    message = 'Calculation length:\t\t' + import_length
-    slackbot(message)
+            result.save()
+
+        end_time = timezone.localtime(timezone.now())
+        end_message = '\nFinished calculations:\t' + str(unicode(end_time)) + '\n'
+        import_length = str(unicode(end_time - start_time)) 
+        message = end_message
+        slackbot(message)
+        message = 'Calculation length:\t\t' + import_length
+        slackbot(message)
 
 def calculate_votepct(electiondate_arg):
-    gdoc_results = ResultManual.objects.filter(electiondate=electiondate_arg, gdoc_import=True)
+    gdoc_results = ResultManual.objects.filter(electiondate=electiondate_arg, gdoc_import=True, votecount__gt=0)
 
-    calculated_total_votes_dict_list = gdoc_results.values('raceid').annotate(vote_count_total=Sum('votecount'))
+    if gdoc_results > 0:
 
-    message = '\n\nVOTE PERCENT'
-    slackbot(message)
-    start_time = timezone.localtime(timezone.now())
-    start_message = 'Started calculations:\t' + str(unicode(start_time))
-    message = start_message
-    slackbot(message)
+        calculated_total_votes_dict_list = gdoc_results.values('raceid').annotate(vote_count_total=Sum('votecount'))
 
-    ## if there are any ResultManual objects that are from a gdoc import
-    if gdoc_results:
-        ## set up a new empty dict 
-        calculated_total_votes_dict = {}
-        ## loop thru the list of dicts with raceids and vote totals to extract and add the relevant key/value pairs
-        for item in calculated_total_votes_dict_list:
-            calculated_total_votes_dict[item['raceid']] = item['vote_count_total']
-        ## loop thru the results
-        for resultmanual_obj in gdoc_results:
-            ## get the raceid
-            resultmanual_obj_race_id = resultmanual_obj.raceid
-            ## get the vote total
-            vote_total = calculated_total_votes_dict[resultmanual_obj_race_id]
-            ## get the vote count
-            vote_count = resultmanual_obj.votecount
-            ## calculate the percent
-            percent_calculated = calculated_percent(vote_count, vote_total)
-            ## get the object id
-            resultmanual_obj_id = resultmanual_obj.id
-            # print str(resultmanual_obj) + ": \t" + str(percent_calculated) + "\n"
-            ## update the object
-            ResultManual.objects.filter(id=resultmanual_obj_id).update(votepct=percent_calculated)
+        message = '\n\nVOTE PERCENT'
+        slackbot(message)
+        start_time = timezone.localtime(timezone.now())
+        start_message = 'Started calculations:\t' + str(unicode(start_time))
+        message = start_message
+        slackbot(message)
 
-    end_time = timezone.localtime(timezone.now())
-    end_message = '\nFinished calculations:\t' + str(unicode(end_time)) + '\n'
-    import_length = str(unicode(end_time - start_time)) 
-    message = end_message
-    slackbot(message)
-    message = 'Calculation length:\t\t' + import_length + '\n'
-    slackbot(message)
+        ## if there are any ResultManual objects that are from a gdoc import
+        if gdoc_results:
+            ## set up a new empty dict 
+            calculated_total_votes_dict = {}
+            ## loop thru the list of dicts with raceids and vote totals to extract and add the relevant key/value pairs
+            for item in calculated_total_votes_dict_list:
+                calculated_total_votes_dict[item['raceid']] = item['vote_count_total']
+            ## loop thru the results
+            for resultmanual_obj in gdoc_results:
+                ## get the raceid
+                resultmanual_obj_race_id = resultmanual_obj.raceid
+                ## get the vote total
+                vote_total = calculated_total_votes_dict[resultmanual_obj_race_id]
+                ## get the vote count
+                vote_count = resultmanual_obj.votecount
+                ## calculate the percent
+                percent_calculated = calculated_percent(vote_count, vote_total)
+                ## get the object id
+                resultmanual_obj_id = resultmanual_obj.id
+                # print str(resultmanual_obj) + ": \t" + str(percent_calculated) + "\n"
+                ## update the object
+                ResultManual.objects.filter(id=resultmanual_obj_id).update(votepct=percent_calculated)
+
+        end_time = timezone.localtime(timezone.now())
+        end_message = '\nFinished calculations:\t' + str(unicode(end_time)) + '\n'
+        import_length = str(unicode(end_time - start_time)) 
+        message = end_message
+        slackbot(message)
+        message = 'Calculation length:\t\t' + import_length + '\n'
+        slackbot(message)
 
 class Command(BaseCommand):
     help = 'Calculate precincts reporting percent for ResultManual object imported from a Google Doc.'
