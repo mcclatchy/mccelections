aaa5e0e38d613d2692df345ef1c5d16d15a7b7af
Greg Linch <greglinch@users.noreply.github.com>
2017-04-24 14:02:11 -0400

    add conditional to check if both the subset and total for calc % change are None

--- electionsproject/results/calculations.py
+++ electionsproject/results/calculations.py
@@ -1,5 +1,8 @@
 ## calculate percent: part divided by whole times 100
 def calculated_percent(subset, total):
-    percent_raw = subset / total
-    percent_calculated = percent_raw * 100
-    return percent_calculated
+    if subset != None and total != None:
+        percent_raw = subset / total
+        percent_calculated = percent_raw * 100
+        return percent_calculated
+    else:
+        print 'No values present to calculate' 
